ce8515e01b079829851db36c34a8879854a9f7fe
hjw1112 <hjw4963@gmail.com>
2024-07-23 01:02:49 +0200

    overall change. card drawing is in function now and old code is in blackj2.cpp

---
+++ blackj/blackj.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <string>
+#include <array>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <vector>
+using namespace std;
+srand(time(NULL));
+
+//card lists
+vector<vector<string>> cards = {
+	{"♠A", "♥A", "♦A", "♣A"},
+	{"♠2", "♥2", "♦2", "♣2"},
+	{"♠3", "♥3", "♦3", "♣3"},
+	{"♠4", "♥4", "♦4", "♣4"},
+	{"♠5", "♥5", "♦5", "♣5"},
+	{"♠6", "♥6", "♦6", "♣6"},
+	{"♠7", "♥7", "♦7", "♣7"},
+	{"♠8", "♥8", "♦8", "♣8"},
+	{"♠9", "♥9", "♦9", "♣9"},
+	{"♠10", "♥10", "♦10", "♣10"},
+	{"♠J", "♥J", "♦J", "♣J"},
+	{"♠Q", "♥Q", "♦Q", "♣Q"},
+	{"♠K", "♥K", "♦K", "♣K"}
+};
+vector<int> cardcord_player;
+vector<int> cardcord_dealer;
+int player_sum;
+int dealer_sum;
+
+pair<int, int> cardgen() {
+
+	//generate random coordinates for selecting card.
+	int Randindex = rand() % 52;
+	int xcord = Randindex / 4;
+	int ycord = Randindex % 4;
+	return make_pair(xcord, ycord);
+}
+
+
+int cardsum(int val1, int val2) {
+	//add up the valuers of card.
+	if (val1 > 9)
+		val1 = 9;
+	if (val2 > 9)
+		val2 = 9;
+	return val1 + val2 + 2;
+};
+
+string draw_card(string who) {
+
+	//draw dealer's card(twice)
+	for (int i = 0; i < 2; i++) {
+		pair<int, int> coords = cardgen();
+		int xcord = coords.first;
+		int ycord = coords.second;
+		string card1 = cards[xcord][ycord];
+		cin.get();
+
+		//put coordinates of cards into "cardcord1" vector(array)
+		cardcord_[who].push_back(xcord);
+		cardcord_[who].push_back(ycord);
+
+
+		return card1;
+	};
+};
+
+
+int main() {
+	cout << "press any key to start";
+	cin.get();
+	cout << "dealer's card: " << draw_card("dealer") << endl;
+	dealer_sum = cardsum(cardcord_dealer[0], cardcord_dealer[2]);
+
+	if (dealer_sum < 17) {
+		draw_card("dealer")
+			dealer_sum = dealer_sum + *(cardcord_dealer.end() - 2) + 1;
+		
+	}
+	cout << dealer_sum << endl;
+
+	cin.get()
+
+		cout << "your card" << draw_card("player") << endl;
+	player_sum = cardsum(cardcord_player[0], cardcord_player[2]);
+	cout << "(total:" << player_sum ")\n";
+
+	char SorH;
+
+	while (sum <= 21) {
+		cout << "Would you stand or hit?(S or H)";
+		cin >> SorH;
+		SorH = tolower(SorH);
+
+		if (SorH == 'h') {
+
+			cout << draw_card("player") << endl
+
+				player_sum = player_sum + *(cardcord_player.end() - 2) + 1;
+
+			cout << "\n(total:";
+			cout << sum;
+			cout << ")\n";
+		}
+
+		else if (SorH == 's') {
+
+			if (dealer_sum < player_sum) {
+				cout << "dealer sum" << dealer_sum;
+				cout << "your sum" << player_sum;
+				cout << "you won";
+			}
+			else {
+				cout << "dealer sum" << dealer_sum << "\n";
+				cout << "your sum" << player_sum << "\n";
+				cout << "dealer won";
+			}
+
+			break;
+		}
+		else {
+			cout << "wrong input\n";
+		}
+
+	}
+
+	if (sum > 21) {
+		cout << "bust(over 21)\n";
+	}
+
+
+
+	return 0;
+}
---
+++ blackj/blackj2.cpp
@@ -0,0 +1,179 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <string>
+#include <array>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <vector>
+using namespace std;
+
+//card lists
+vector<vector<string>> cards = {
+	{"♠A", "♥A", "♦A", "♣A"},
+	{"♠2", "♥2", "♦2", "♣2"},
+	{"♠3", "♥3", "♦3", "♣3"},
+	{"♠4", "♥4", "♦4", "♣4"},
+	{"♠5", "♥5", "♦5", "♣5"},
+	{"♠6", "♥6", "♦6", "♣6"},
+	{"♠7", "♥7", "♦7", "♣7"},
+	{"♠8", "♥8", "♦8", "♣8"},
+	{"♠9", "♥9", "♦9", "♣9"},
+	{"♠10", "♥10", "♦10", "♣10"},
+	{"♠J", "♥J", "♦J", "♣J"},
+	{"♠Q", "♥Q", "♦Q", "♣Q"},
+	{"♠K", "♥K", "♦K", "♣K"}
+};
+int sum;
+
+pair<int, int> carddist() {
+
+	//generate random coordinates for selecting card.
+	int Randindex = rand() % 52;
+	int xcord = Randindex / 4;
+	int ycord = Randindex % 4;
+	return make_pair(xcord, ycord);
+}
+
+int cardsum(int val1, int val2) {
+	//add up the valuers of card.
+	if (val1 > 9)
+		val1 = 9;
+	if (val2 > 9)
+		val2 = 9;
+	return val1 + val2 + 2;
+};
+
+//string dealer() {
+
+	//pair<int, int> coords = carddist();
+	//int xcord = coords.first;
+	//int ycord = coords.second;
+	//string card = cards[xcord][ycord];
+
+	//return card;
+//}
+
+
+
+
+int main() {
+	cout << "press any key to start";
+	cin.get();
+
+	vector<int> cardcord1;
+	//draw dealer's card
+	for (int i = 0; i < 2; i++) {
+		pair<int, int> coords = carddist();
+		int xcord = coords.first;
+		int ycord = coords.second;
+		string card1 = cards[xcord][ycord];
+		cin.get();
+
+		//put coordinates of cards into "cardcord" vector(array)
+		cardcord1.push_back(xcord);
+		cardcord1.push_back(ycord);
+
+		cout << "dealer's card ";
+		cout << card1 << endl;
+	}
+
+
+
+	cin.get();
+
+
+
+
+	vector<int> cardcord;
+	srand(time(NULL));
+
+	//print random card
+	cout << "your card: \n";
+	for (int i = 0; i < 2; i++) {
+		pair<int, int> coords = carddist();
+		int xcord = coords.first;
+		int ycord = coords.second;
+		string card = cards[xcord][ycord];
+		cout << card;
+		cin.get();
+
+		//put coordinates of cards into "cardcord" vector(array)
+		cardcord.push_back(xcord);
+		cardcord.push_back(ycord);
+	};
+
+	sum = cardsum(cardcord[0], cardcord[2]);
+
+	cout << "(total:";
+	cout << sum;
+	cout << ")\n";
+
+
+	char resp;
+	resp = 'h';
+
+
+	//next options
+	while (sum <= 21) {
+		cout << "Would you stand or hit?(S or H)";
+		cin >> resp;
+		resp = tolower(resp);
+
+		if (resp == 'h') {
+
+			pair<int, int> coords = carddist();
+			int xcord = coords.first;
+			int ycord = coords.second;
+			string card = cards[xcord][ycord];
+			cout << card;
+
+			sum = sum + xcord + 1;
+
+			cout << "\n(total:";
+			cout << sum;
+			cout << ")\n";
+		}
+		else if (resp == 's') {
+			dcsum = cardsum(cardocord1[0], cardcord1[2]);
+
+			if (dcsum < sum) {
+				cout << dcsum;
+				cout << sum;
+			}
+			else {
+				cout << dcsum;
+				cout << sum;
+			}
+
+			break;
+		}
+		else {
+			cout << "wrong input\n";
+		}
+
+	}
+
+	if (sum > 21) {
+		cout << "bust(over 21)\n";
+	}
+
+	"dealer's card";
+
+
+	return 0;
+}
